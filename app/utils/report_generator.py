46d37b08c665b98a6ac4bee8b6abc71a93199159
amardom <semprecelta@gmail.com>
2026-01-18 09:05:23 +0100

    Estetica

--- app/utils/report_generator.py
+++ app/utils/report_generator.py
@@ -2,6 +2,7 @@ from fpdf import FPDF
 import os
 
 def generate_minimalist_report(data, output_path):
+    
     # P = Portrait, mm = millimeters, A4
     pdf = FPDF('P', 'mm', 'A4')
     pdf.add_page()
--- main.py
+++ main.py
@@ -3,19 +3,20 @@ from app.utils.report_generator import generate_minimalist_report
 from app.core.z3_engine import auditor_z3_pro
 
 def main():
-    # 1. Cargar el JSON de lógica (el que validamos antes)
+    
+    # 1. Load json with vars and logics.
     with open("data/samples/PDFSOLUTIONS/vars_and_logics_PDFSOLUTIONS.json", "r") as f:
         logic_data = json.load(f)
 
-    # 2. Generar el PDF para el CFO
+    # 2. Generate report from json.
     output_pdf = "audit_report_CFO.pdf"
     generate_minimalist_report(logic_data, output_pdf)
-    
-    print(f"Reporte generado exitosamente en: {output_pdf}")
+    print(f"Report successfully generated in: {output_pdf}")
 
-    # 3. Leemos datos del CFO.
+    # 3. Read CFO data.
     cfo_data = json.load(open('data/samples/PDFSOLUTIONS/cfo_data_PDFSOLUTIONS.json'))
 
+    # 4. Verify logics.
     auditor_z3_pro(logic_data, cfo_data)
 
 if __name__ == "__main__":
