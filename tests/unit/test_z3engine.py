7b384a553543e2067ae9bb64f7435349c76dc7ee
amardom <semprecelta@gmail.com>
2026-01-19 14:07:41 +0100

    Rigidizar el testing.

--- tests/unit/test_z3engine.py
+++ tests/unit/test_z3engine.py
@@ -10,11 +10,10 @@ def test_all_scenarios():
     
     scenarios = [
         ("logic_data_pass_simple.json","cfo_data_pass_simple.json", True, 13085595.147),
-        ("logic_data_pass_complex.json","cfo_data_pass_complex.json", True, 15493715.317),
-        ("logic_data_pass_complex.json","cfo_data_pass_complex_fail.json", False, 0.0)
+        ("logic_data_pass_complex.json","cfo_data_pass_complex.json", True, 15493715.317)
     ]
 
-    print(f"\n--- INITIALIZING TESTING LOOP ---")
+    print(f"\n--- INITIALIZING SAT TESTING LOOP ---")
 
     for filename_logic, filename_cfo, expected_sat_unsat, expected_norm_metric in scenarios:
 
@@ -30,3 +29,26 @@ def test_all_scenarios():
 
         print(f"Testing {filename_logic}: Expected {expected_norm_metric} -> Got {result['norm_metric']}")
         assert result["norm_metric"] == pytest.approx(expected_norm_metric), f"Error in {filename_logic}: Result does not match."
+
+def test_unsat_scenarios():
+    
+    scenarios = [
+        ("logic_data_pass_complex.json","cfo_data_pass_complex_fail.json", False, ['consolidated_total_net_leverage_ratio', 'unrestricted_cash'])
+    ]
+
+    print(f"\n--- INITIALIZING UNSAT TESTING LOOP ---")
+
+    for filename_logic, filename_cfo, expected_sat_unsat, expected_conflict_variables in scenarios:
+
+        path_logic_data = f"tests/scenarios/{filename_logic}"
+        logic_data = load_json(path_logic_data)
+        path_cfo_data = f"tests/scenarios/{filename_cfo}"
+        cfo_data = load_json(path_cfo_data)
+        
+        result = verify_logics(logic_data, cfo_data)
+        
+        print(f"Testing {filename_logic}: Expected {expected_sat_unsat} -> Got {result['is_compliant']}")
+        assert result["is_compliant"] == expected_sat_unsat, f"Error in {filename_logic}: Result does not match."
+
+        print(f"Testing {filename_logic}: Expected {expected_conflict_variables} -> Got {result['conflict_variables']}")
+        assert set(result["conflict_variables"]) == set(expected_conflict_variables), f"Error in {filename_logic}: Result does not match."
