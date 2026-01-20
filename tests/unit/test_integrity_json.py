991d5e0524f24ea443e12dd6bfc0dd8f27fd425e
amardom <semprecelta@gmail.com>
2026-01-20 23:44:49 +0100

    Blindando el codigo.

--- tests/unit/test_integrity_json.py
+++ tests/unit/test_integrity_json.py
@@ -24,6 +24,18 @@ def test_validate_json_empty_source_file():
     with pytest.raises(AssertionError):
         validate_json(FILENAME, logics)
 
+def test_validate_json_type_source_file():
+    logics = VALID_LOGICS.copy()
+    logics["source_file"] = True
+    with pytest.raises(AssertionError):
+        validate_json(FILENAME, logics)
+
+def test_validate_json_wrong_filename():
+    logics = VALID_LOGICS.copy()
+    logics["source_file"] = "wrong_name.json"
+    with pytest.raises(AssertionError):
+        validate_json(FILENAME, logics)
+
 def test_validate_json_empty_contract_name():
     logics = VALID_LOGICS.copy()
     logics["contract_name"] = []
@@ -31,9 +43,10 @@ def test_validate_json_empty_contract_name():
     with pytest.raises(AssertionError):
         validate_json(FILENAME, logics)
 
-def test_validate_json_wrong_filename():
+def test_validate_json_type_contract_name():
     logics = VALID_LOGICS.copy()
-    logics["source_file"] = "wrong_name.json"
+    logics["contract_name"] = True
+    print(logics["contract_name"])
     with pytest.raises(AssertionError):
         validate_json(FILENAME, logics)
 
@@ -60,6 +73,16 @@ def test_validate_json_missing_keys_in_variable():
     with pytest.raises(AssertionError):
         validate_json(FILENAME, logics)
 
+def test_validate_json_types_in_variable():
+    logics = VALID_LOGICS.copy()
+    for var in logics["variables"]:
+        var["name"] = True
+        with pytest.raises(AssertionError):
+            validate_json(FILENAME, logics)
+        var["context"] = True
+        with pytest.raises(AssertionError):
+            validate_json(FILENAME, logics)
+
 def test_validate_json_missing_keys_in_logical_conditions():
     logics = VALID_LOGICS.copy()
     logics["logical_conditions"] = [{"id": 1, "formula": "ratio <= 2.1", "evidence": "text"}] # Missing 'page'
@@ -80,3 +103,19 @@ def test_validate_json_missing_keys_in_logical_conditions():
     logics["logical_conditions"] = [{"formula": "ratio <= 2.1", "evidence": "text", "page": 31}] # Missing 'id'
     with pytest.raises(AssertionError):
         validate_json(FILENAME, logics)
+
+def test_validate_json_types_in_logical_conditions():
+    logics = VALID_LOGICS.copy()
+    for var in logics["logical_conditions"]:
+        var["id"] = "1"
+        with pytest.raises(AssertionError):
+            validate_json(FILENAME, logics)
+        var["evidence"] = True
+        with pytest.raises(AssertionError):
+            validate_json(FILENAME, logics)
+        var["formula"] = True
+        with pytest.raises(AssertionError):
+            validate_json(FILENAME, logics)
+        var["formula"] = "1"
+        with pytest.raises(AssertionError):
+            validate_json(FILENAME, logics)
