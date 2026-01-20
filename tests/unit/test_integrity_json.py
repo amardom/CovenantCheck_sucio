e0b81928481de38df405a28dca3ffbaa57c02ffa
amardom <semprecelta@gmail.com>
2026-01-20 19:58:11 +0100

    Avanzamos con los tests.

--- tests/unit/test_integrity_json.py
+++ tests/unit/test_integrity_json.py
@@ -3,7 +3,7 @@ from app.utils.integrity_json import validate_json
 
 FILENAME = "logics_NETFLIX.json"
 
-VALID_DATA = {
+VALID_LOGICS = {
     "source_file": FILENAME,
     "variables": [{"name": "ebitda", "context": "LTM"}],
     "logical_conditions": [{
@@ -16,50 +16,59 @@ VALID_DATA = {
 
 def test_validate_json_success():
     """Should pass without raising any AssertionError."""
-    validate_json(FILENAME, VALID_DATA)
+    validate_json(FILENAME, VALID_LOGICS)
 
 def test_validate_json_wrong_filename():
     """Should explode if the source_file doesn't match the actual filename."""
-    data = VALID_DATA.copy()
+    data = VALID_LOGICS.copy()
     data["source_file"] = "wrong_name.json"
     with pytest.raises(AssertionError):
         validate_json(FILENAME, data)
 
 def test_validate_json_empty_variables():
     """Should explode if variables list is empty."""
-    data = VALID_DATA.copy()
+    data = VALID_LOGICS.copy()
     data["variables"] = []
     with pytest.raises(AssertionError):
         validate_json(FILENAME, data)
 
 def test_validate_json_empty_logical_conditions():
     """Should explode if variables list is empty."""
-    data = VALID_DATA.copy()
+    data = VALID_LOGICS.copy()
     data["logical_conditions"] = []
     with pytest.raises(AssertionError):
         validate_json(FILENAME, data)
 
-def test_validate_json_missing_fields_in_variable():
+def test_validate_json_missing_keys_in_variable():
     """Should explode if a variable is missing the 'name' or 'context' key."""
-    data = {
-        "source_file": FILENAME,
-        "variables": [{"name": "ebitda"}], # Missing 'context'
-        "logical_conditions": VALID_DATA["logical_conditions"]
-    }
+    data = VALID_LOGICS.copy()
+    data["variables"] = [{"name": "ebitda"}], # Missing 'context'
     with pytest.raises(AssertionError):
         validate_json(FILENAME, data)
 
-    data = {
-    "source_file": FILENAME,
-    "variables": [{"context": "income before itda"}], # Missing 'name'
-    "logical_conditions": VALID_DATA["logical_conditions"]
-    }
+    data = VALID_LOGICS.copy()
+    data["variables"] = [{"context": "income before itda"}], # Missing 'name'
     with pytest.raises(AssertionError):
         validate_json(FILENAME, data)
 
-def test_validate_json_missing_logical_conditions_keys():
+def test_validate_json_missing_keys_in_logical_conditions():
     """Should explode if a logical condition is missing required keys like 'formula'."""
-    data = VALID_DATA.copy()
-    data["logical_conditions"] = [{"id": 1, "evidence": "text"}] # Missing 'formula' and 'page'
+    data = VALID_LOGICS.copy()
+    data["logical_conditions"] = [{"id": 1, "formula": "ratio <= 2.1", "evidence": "text"}] # Missing 'page'
+    with pytest.raises(AssertionError):
+        validate_json(FILENAME, data)
+
+    data = VALID_LOGICS.copy()
+    data["logical_conditions"] = [{"id": 1, "formula": "ratio <= 2.1", "page": 31}] # Missing 'evidence'
+    with pytest.raises(AssertionError):
+        validate_json(FILENAME, data)
+
+    data = VALID_LOGICS.copy()
+    data["logical_conditions"] = [{"id": 1, "evidence": "text", "page": 31}] # Missing 'formula'
+    with pytest.raises(AssertionError):
+        validate_json(FILENAME, data)
+    
+    data = VALID_LOGICS.copy()
+    data["logical_conditions"] = [{"formula": "ratio <= 2.1", "evidence": "text", "page": 31}] # Missing 'id'
     with pytest.raises(AssertionError):
         validate_json(FILENAME, data)
